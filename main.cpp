790d25dfcbed719e5387cdb42574a8d1bb870092
Hrishik <116365046+fringewidth@users.noreply.github.com>
2024-10-15 17:38:51 +0530

    Back to the drawing board

--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
-int main() {};
+#include "include/mySTL/array.h"
+#include<iostream>
+
+using namespace std;
+
+int main () {
+    array<int, 5> arr;
+    arr[4] = 541;
+    cout<<*(arr.get_back())<<endl;
+}
